97ee9f1c95b657c57ab4686fa48038d24927bc65
Andrew M <fileformat@gmail.com>
2013-05-26 09:16:37 -0400

    updated thumbnails

--- bin/mkthumbs.sh
+++ bin/mkthumbs.sh
@@ -3,10 +3,16 @@
 # regenerate thumbnails
 #
 #
-ORIG=../../rosetta-data
-./_mkthumb.sh ${ORIG}/1903-firstflight.tif firstflight
-./_mkthumb.sh ${ORIG}/1969-earthrise.tif earthrise
-./_mkthumb.sh ${ORIG}/1972-pioneer10.svg pioneer10
-./_mkthumb.sh ${ORIG}/1977-voyagercover.jpeg voyager
-./_mkthumb.sh ../thumbnail/adultschool.jpeg adultschool
-./_mkthumb.sh ../thumbnail/firstnames.png firstnames
+ORIG=../../rosetta-key/www
+#./_mkthumb.sh ${ORIG}/1903-firstflight.tif firstflight
+#./_mkthumb.sh ${ORIG}/1969-earthrise.tif earthrise
+#./_mkthumb.sh ${ORIG}/1972-pioneer10.svg pioneer10
+#./_mkthumb.sh ${ORIG}/1977-voyagercover.jpeg voyager
+#./_mkthumb.sh ../thumbnail/adultschool.jpeg adultschool
+#./_mkthumb.sh ../thumbnail/firstnames.png firstnames
+#./_mkthumb.sh ../thumbnail/gettysburg.jpeg gettysburg
+#./_mkthumb.sh ${ORIG}/1799-rosettastone.jpeg rosettastone
+#./_mkthumb.sh ${ORIG}/1972-lenna.tiff lenna
+#./_mkthumb.sh ../thumbnail/independence.jpeg independence
+#./_mkthumb.sh ${ORIG}/2002-tiger.eps tiger
+#./_mkthumb.sh ../thumbnail/smallstep.jpeg smallstep
